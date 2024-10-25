e721cc76bac74f16fe14b68562ad2e375ebb12be
siya ahlawat <siyaahlawat1210@gmail.com>
2024-10-25 23:35:55 +0530

    Add files via upload

---
+++ Potfolio1/script.js
@@ -0,0 +1,18 @@
+// Handle Email Subscription
+// document.getElementById('emailForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     const email = this.querySelector('input[type="email"]').value;
+//     // TODO: Add integration with Google Sheets here
+//     alert('Thank you for subscribing with: ' + email);
+// });
+
+// Handle Contact Form Submission
+// document.getElementById('contactForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     const name = this.querySelector('input[type="text"]').value;
+//     const email = this.querySelector('input[type="email"]').value;
+//     const phone = this.querySelector('input[type="tel"]').value;
+//     const message = this.querySelector('textarea').value;
+//     // TODO: Add integration with Google Sheets her
+//     alert('Thank you for contacting us, ' + name + '!');
+// });
